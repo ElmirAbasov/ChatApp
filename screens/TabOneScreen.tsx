50d2b0be65228d120db6369f16a7050f16d835cb
Xizafren <56082840+Xizafren@users.noreply.github.com>
2021-09-03 15:13:41 +0200

    First Commit with mockdata

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,63 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { Text, View, StyleSheet, Image } from "react-native";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
+    //Inline styling
+
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",}}style={styles.image}/>
+      <View style={styles.rightContainer}>
+          <View style={styles.row}>
+            <Text style={styles.name}>Elon Musk</Text>
+            <Text style={styles.text}>11:11am</Text>
+          </View>
+          <Text numberOfLines={1} style={styles.text}> Eniki Beniki sudakamo teus beus kuzma teuz box</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    padding: 10,
+
+
+  },
+  image: {
+    height: 50,
+    width: 50,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+
+  rightContainer: {
+   
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+
+
   },
-  title: {
-    fontSize: 20,
+
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
+    
+  },
+  name: {
     fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 3,
+
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+
+  text: {
+    
+    color: "grey",
   },
 });
